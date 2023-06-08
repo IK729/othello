5ab310ca4ab578e6a961ee83d16f3b5ed717e246
1f10230097 <s1f102300975@iniad.org>
2023-06-09 01:51:56 +0900

    あ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,38 +2,47 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [turnColor, setTurnColor]= useState(1)
-  const [board,setBoard]=useState([
-  [0,0,0,0,0,0,0,0,],
-  [0,0,0,0,0,0,0,0,],
-  [0,0,0,0,0,0,0,0,],
-  [0,0,0,1,2,0,0,0,],
-  [0,0,0,2,1,0,0,0,],
-  [0,0,0,0,0,0,0,0,],
-  [0,0,0,0,0,0,0,0,],
-  [0,0,0,0,0,0,0,0,]
+  const [turnColor, setTurnColor] = useState(1);
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const onClick=(x:number,y:number)=>{
+  const onClick = (x: number, y: number) => {
     {
-   console.log(x,y)
-   const newBoard = JSON.parse(JSON.stringify(board));
-   if(board [y + 1]!== undefined && board[y + 1][x]!==0 && board[y + 1][x]!==turnColor){
-    newBoard[y][x]=turnColor
-    setTurnColor(3 - turnColor);
+      console.log(x, y);
+      const newBoard = JSON.parse(JSON.stringify(board));
+      if (
+        board[y + 1] !== undefined &&
+        board[y + 1][x] === 3 - turnColor &&
+        board[y + 1][x] !== turnColor
+      ) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(3 - turnColor);
       }
-    setBoard(newBoard)
-  }
-  }
+      setBoard(newBoard);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-      {board.map((row,y)=>
-       row.map((color,x)=>        
-        <div className={styles.cell} key = {`${x}-${y}`} onClick={()=> onClick(x,y)}>
-          {color !==0 &&<div className={styles.stone}style={{background:color===1?'#000':'#fff'}}/>}
-        </div>
-       ))
-       }
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
