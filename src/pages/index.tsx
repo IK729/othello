2e68fa310fa0b16955849787d580de19b136809b
IK729 <s1f102300975@iniad.org>
2023-06-24 11:47:01 +0900

    23/6/24 AM

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,26 +14,36 @@ const Home = () => {
     //   [0, 0, 0, 0, 0, 0, 0, 0],
     //   [0, 0, 0, 0, 0, 0, 0, 0],
     // ]
-    [
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 0, 0, 2, 1],
-      [0, 0, 0, 0, 0, 1, 2, 1],
-      [0, 0, 0, 0, 2, 1, 2, 1],
-      [0, 0, 0, 1, 2, 1, 2, 1],
-      [0, 0, 2, 1, 2, 1, 2, 1],
-      [0, 2, 1, 2, 1, 2, 1, 2],
-    ]
     // [
     //   [0, 0, 0, 0, 0, 0, 0, 0],
-    //   [2, 2, 1, 2, 1, 2, 1, 0],
-    //   [1, 1, 2, 1, 2, 1, 2, 0],
-    //   [2, 2, 1, 2, 1, 2, 1, 0],
-    //   [1, 1, 2, 1, 2, 1, 2, 0],
-    //   [2, 2, 1, 2, 1, 2, 1, 0],
-    //   [1, 1, 2, 1, 2, 1, 2, 0],
-    //   [2, 1, 2, 1, 2, 1, 2, 0]
+    //   [0, 0, 0, 0, 0, 0, 0, 1],
+    //   [0, 0, 0, 0, 0, 0, 2, 1],
+    //   [0, 0, 0, 0, 0, 1, 2, 1],
+    //   [0, 0, 0, 0, 2, 1, 2,1],
+    //   [0, 0, 0, 1, 2, 1, 2, 1],
+    //   [0, 0, 2, 1, 2, 1, 2, 1],
+    //   [0, 2, 1, 2, 1, 2, 1, 2],
     // ]
+    // [
+    //   [0, 0, 0, 0, 0, 0, 0, 0],
+    //   [2, 1, 0, 0, 0, 0, 0, 0],
+    //   [0, 0, 0, 0, 0, 0, 0, 0],
+    //   [1, 1, 0, 0, 0, 0, 0, 0],
+    //   [0, 2, 0, 0, 0, 0, 0, 0],
+    //   [0, 0, 0, 0, 0, 0, 0, 0],
+    //   [0, 0, 0, 0, 0, 0, 0, 0],
+    //   [0, 0, 0, 0, 0, 0, 0, 0],
+    // ]
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [2, 2, 1, 2, 1, 2, 1, 0],
+      [1, 1, 2, 1, 2, 1, 2, 0],
+      [2, 2, 1, 2, 1, 2, 1, 0],
+      [1, 1, 2, 1, 2, 1, 2, 0],
+      [2, 2, 1, 2, 1, 2, 1, 0],
+      [1, 1, 2, 1, 2, 1, 2, 0],
+      [2, 1, 2, 1, 2, 1, 2, 0],
+    ]
     //   [
     //   [0, 0, 0, 0, 0, 0, 0, 0],
     //   [1, 1, 1, 1, 1, 1, 1, 0],
@@ -46,22 +56,95 @@ const Home = () => {
     // ]
   );
   const onClick = (x: number, y: number) => {
-    {
-      console.log(x, y);
-      const newBoard = JSON.parse(JSON.stringify(board));
-      if (
-        board[y + 1] !== undefined &&
-        board[y + 1][x] === 3 - turnColor &&
-        board[y + 1][x] !== turnColor
-      ) {
-        newBoard[y][x] = turnColor;
-        setTurnColor(3 - turnColor);
-      }
-      // if (
+    console.log(x, y);
+    const newBoard = JSON.parse(JSON.stringify(board));
 
-      // )
-      setBoard(newBoard);
+    const directions = [
+      [-1,1],
+      [0,1],
+      [1,1],
+      [1,0],
+      [1,-1],
+      [0,-1],
+      [-1,-1],
+      [-1,0]
+  ]
+
+  for (const d of directions) {
+
+    if (board[y + d[1]] !== undefined && 
+    board[y + d[1]][x + d[0]] === 3 - turnColor) {
+      for (let n = 2; n < 8; n += 1) {
+        if (board[(y + d[1]) * n] !== undefined) {
+          if (board[(y + d[1]) * n][(x + d[0]) * n] === turnColor) {
+            newBoard[y][x] = turnColor;
+            setTurnColor(3 - turnColor);
+
+            for (let m = n - d[0]; m > 0; m -= 1) {
+              newBoard[(y + d[1]) * m][(x + d[0]) * m] = turnColor;
+            }
+
+            setBoard(newBoard);
+            break;
+          } else if (board[(y + d[1]) * n][(x + d[0]) * n] === 3 - turnColor) {
+            continue;
+          }
+        }
+      }
+        break;
+      }
     }
+    // if(
+    //   board[y + 1] !== undefined &&
+    //   board[y + 1][x] === 3 - turnColor
+    // ) {
+    //   for(let n = 2; n < 8; n += 1) {if(
+    //     board[y + n] !== undefined &&
+    //     board[y + n][x] === turnColor) {
+    //       newBoard[y][x] = turnColor
+    //       setTurnColor(3 - turnColor)
+
+    //       for(let m = n - 1; m > 0; m -= 1) {
+    //         newBoard[y + m][x] = turnColor
+    //       }
+
+    //       setBoard(newBoard);
+    //       break
+    //     }
+    //     else if(
+    //       board[y + n] !== undefined &&
+    //       board[y + n][x] === 3 - turnColor
+    //     ) {
+    //       continue
+    //     }
+    //     break
+    //   }
+    // }
+
+    // if (
+    //   board[y + 1] !== undefined &&
+    //   board[y + 1][x] === 3 - turnColor
+    // ) {
+    //   if (
+    //     board[y + 2] !== undefined &&
+    //     board[y + 2] [x] === turnColor
+    //   ) {
+    //     newBoard[y][x] = turnColor
+    //     setTurnColor(3 - turnColor)
+    //   }else if(
+    //     board[y + 2] !== undefined &&
+    //     board[y + 2] [x] === 3 - turnColor
+    //   ) {
+    //    if(
+    //     board[y + 3] !== undefined &&
+    //     board[y + 3] [x] === turnColor
+    //    ) {
+    //     newBoard[y][x] = turnColor
+    //     setTurnColor(3 - turnColor)
+    //    }
+    //   }
+    //   setBoard(newBoard);
+    // }
   };
   return (
     <div className={styles.container}>
